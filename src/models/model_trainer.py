414f771b1660412fad2b6d0e8e173b46e0306cf4
yasvanthkohli <yasvanthkohli@gmail.com>
2025-07-23 23:49:05 +0530

    basic ModelTrainer class

--- src/models/model_trainer.py
+++ src/models/model_trainer.py
@@ -1,1 +1,9 @@
-# model_trainer.py
+from sklearn.linear_model import LogisticRegression
+
+class ModelTrainer:
+    def __init__(self):
+        self.model = LogisticRegression(max_iter=1000)
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+        return self.model
